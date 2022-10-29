07f78eebde63e0dab35ef2f2e74114afd66c8b20
AHZU DIRECT TECH LLC <ahzudirect@gmail.com>
2022-10-28 23:38:50 -0700

    Title

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,9 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Muse Defi Network | Open Music Collective</title>
-        <meta name="description" content=" An Open Music Label project that combines the idea of Music Marketing and Defi." />
+        <meta name="description" content=" An Open Music Label project that combines the idea of Music Distribution, Marketing and Defi." />
         <link rel="icon" href="/favicon.ico" />
+
       </Head>
 
       <main className={styles.main}>
@@ -28,7 +29,7 @@ export default function Home() {
           </div>
           <div>
               <h1 className={styles.title}>
-          MUSE DEFI <span></span>
+          MUSE DEFI <span>NETWORK</span>
           </h1>
           </div>
         
@@ -36,36 +37,6 @@ export default function Home() {
 
 
         <div className={styles.grid}>
-          <a href="https://opencollective.com/muse-defi-network" className={styles.card}>
-            <h2>Open Collective &rarr;</h2>
-            <p>Become a Contributor and join a thriving community of creators. </p>
-          </a>
-
-          <a href="https://chat.whatsapp.com/E4onwdYTQMs5RLhAIzhmL5" className={styles.card}>
-            <h2>WhatsApp &rarr;</h2>
-            <p>Connect & vibe with like minded members and get support.</p>
-          </a>
-
-          <a
-            href="https://github.com/Muse-Defi-Network/landing-page"
-            className={styles.card}
-          >
-            <h2>GitHub &rarr;</h2>
-            <p>Clone the repo for this page and deploy in you local environment.</p>
-          </a>
-
-          <a
-            href="https://www.ahzudirecttech.com/"
-            className={styles.card}
-          >
-            <h2>Contact &rarr;</h2>
-            <p>
-              Reach out to us concerning inquires or feature requests.
-            </p>
-          </a>
-        </div>
-
-        <div className={styles.grid-2}>
           <a href="https://opencollective.com/muse-defi-network" className={styles.card}>
             <h2>Open Collective &rarr;</h2>
             <p>Become a Contributor and join a thriving community of creators. </p>
@@ -102,7 +73,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          &copy; 2022 Muse Defi Network | Protected by <span className={styles.ftitle}> Open Collective
+          &copy; 2022 Muse Defi Network <span className={styles.ftitle}>
           </span>
         </a>
       </footer>
